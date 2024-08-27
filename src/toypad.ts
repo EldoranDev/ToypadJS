9da44e3ce5745773b0f02a92acde1572186f0618
Marcel Behrmann <info@eldoran.dev>
2024-08-27 13:15:00 +0200

    Start work on event handling

--- src/toypad.ts
+++ src/toypad.ts
@@ -1,52 +1,61 @@
 import { Pad } from "./pad";
-import { INIT, ProductID, VendorID } from "./usb";
+import { Command, INIT, ProductID, VendorID } from "./usb";
 
 type Color = [r: number, g: number, b: number];
 
-export class Toypad {
-    private isInit: boolean = false;
+export class Toypad extends EventTarget {
+    get open(): boolean {
+        return this.device.opened;
+    }
 
-    private listener: Function|null = null;
-
-    constructor(
+    private constructor(
         private device: HIDDevice
     ) {
-        
+        super();
     }
 
     public async init(): Promise<void> {
-        if (this.isInit) {
+        if (this.open) {
             throw new Error("Already initialised");
         }
 
         await this.device.open();
-        // await this.device.selectConfiguration(1);
-        // await this.device.claimInterface(0);
 
         await this.device.sendReport(0, INIT);
 
         this.device.addEventListener("inputreport", (event) => {
-            console.log("RX: ", [...new Uint8Array(event.data.buffer)].map((x) => x.toString(16).padStart(2, '0')).join(' '));
+            const data = new Uint8Array(event.data.buffer);
+
+            console.log("RX: ", [...data].map((x) => x.toString(16).padStart(2, '0')).join(' '));
+        
+            switch (data[1]) {
+                case Command.MinifigScan:
+                    const eventData = {
+                        panel: data[2],
+                        index: data[4],
+                        action: data[5],
+                        uid: data.slice(7, 13),
+                    };
+
+                    this.dispatchEvent(new CustomEvent("minifig-scan", { detail: eventData }))            
+                    break;
+            }
         });
     }
 
     public async setColor(pad: Pad, [r, g, b]: Color): Promise<void> {
-        if (!this.device.opened) {
-            throw new Error("Device is not open");
-        }
-
         this.send([0x55, 0x06, 0xC0, 0x02, pad, r, g, b]);
     }
 
     private async send(command: Array<number>) {
+        if (!this.open) {
+            throw new Error("The Device is not open");
+        }
+
         let checksum = 0;
 
         for (const word of command) {
-            checksum += word;
-
-            if (checksum >= 256) {
-                checksum -= 256;
-            }
+            checksum = (checksum + word) & 0xFF;
         }
 
         command.push(checksum);
